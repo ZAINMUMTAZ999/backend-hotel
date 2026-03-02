8b4498c6d6a3de1df476befd2e608fe07a54bdaa
Muhammad Zain Mumtaz <mzainmumtaz99@gmail.com>
2026-03-02 17:34:03 +0500

    A

--- src/routes/loginRouter.ts
+++ src/routes/loginRouter.ts
@@ -101,7 +101,7 @@ loginRouter.get(
   "/data",
 
   async (_req: Request, resp: Response) => {
-    resp.status(200).json({ message: "hey data api working backend getting" });
+    resp.status(200).json({ message: "hey data api working a" });
   }
 );
 // getting id per hotel
