fa8f35c2b89ce373c32a4e3eea8b7d0e41f85f87
Muhammad Zain Mumtaz <mzainmumtaz99@gmail.com>
2026-03-02 14:17:43 +0500

    api

---
+++ src/index.ts
@@ -0,0 +1,76 @@
+import express ,{Response, Router} from "express";
+// import "dotenv/config";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import serverless from "serverless-http";
+import mongoose from "mongoose";
+import { registerRouter } from "./routes/registerRouter";
+import { loginRouter } from "./routes/loginRouter";
+import { v2 as cloudinary } from "cloudinary";
+import { addHeroImageRouter } from "./routes/addHeroImageRouter";
+import dotenv from "dotenv";
+dotenv.config();
+const MONGODB_URL = "mongodb+srv://mzainmumtaz99_db_user:yf3e6r4ANk545NTk@cluster0.245yfua.mongodb.net/?appName=Cluster0";
+
+const app = express();
+app.use(express.json());
+
+mongoose
+  .connect(MONGODB_URL)
+  .then(() => console.log("Database connected successfully"))
+  .catch((error) => {
+    console.error("Database connection error:", error);
+  });
+const allowedOrigins = [
+  "https://demosekaispacehotelapp.vercel.app",
+  "http://localhost:3000"
+
+];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+// app.use(
+//   cors({
+//     origin: "https://demosekaispacehotelapp.vercel.app",
+ 
+
+//     credentials: true,
+//   })
+// );
+// 
+// const CLOUDINARY_API_SECRET=oh1XJ4ucXibvjBGywHQ_vkXRQv4
+// const CLOUDINARY_API_KEY=227999443996848
+// const CLOUDINARY_CLOUD_NAME=Zain_Choudhary
+cloudinary.config({
+  cloud_name: "oh1XJ4ucXibvjBGywHQ_vkXRQv4",
+  api_key: "227999443996848",
+  api_secret: "Zain_Choudhary",
+});
+
+const router = Router();
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+router.use("/v1", registerRouter);
+router.use("/v2", loginRouter);
+router.use("/v3", addHeroImageRouter);
+
+
+app.get("/", (_req, res:Response) => {
+  res.send("✅ Backend running successfully !");
+});
+const handler = serverless(app);
+console.log("handler",handler);
+export { handler };
+app.listen(8000,()=>{
+  console.log("app is 8000")
+})
